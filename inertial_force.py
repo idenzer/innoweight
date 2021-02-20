35310276663ce8ba5be68935a4e67365b35de173
Jack Morton <jackm767@gmail.com>
2021-02-20 11:25:19 -0500

    Create inertial_force.py
    Created basic inertial calculation

---
+++ inertial_force.py
@@ -0,0 +1,31 @@
+import odrive
+
+print("Configuring odrive")
+odrv0 = odrive.find_any(timeout=10)
+if str(odrv0) == "None":
+    print("Didn't find an odrive :(")
+else:
+    print("Found an odrive!")
+    print("Bus voltage is: ", str(odrv0.vbus_voltage))
+
+g=9.8
+first_reduction = 4.8
+spool_width = .1524 #.127 if 5in can't remember
+
+while True:
+	
+	#some acceleration that is to be read in depending on how we implement it
+	accel = 1
+
+	#user input weight
+	desired_weight = 100
+
+	force = desired_weight*g + desired_weight*g*accel
+
+	spool_torque = force/.1524
+
+	motor_torque = spool_torque/first_reduction
+
+	current = motor_torque/kv
+
+	odrv0.axis0.motor.config.current_lim = current
